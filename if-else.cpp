90a5ac6e0aecda511183c38149b83fbdf0aabb58
Pulkitkhanna <63971237+pulkitkhanna1@users.noreply.github.com>
2022-01-31 22:12:40 +0530

    Create if-else.cpp

---
+++ if-else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"enter 1st number ";
+    cin>>a;
+    cout<<"enter 2nd number ";
+    cin>>b;
+    
+    if (a<b){
+        cout<<"1st number is smaller";
+        
+    }else if (b<a){
+        cout<<"2nd number is smaller";
+    
+}
+    
+}
